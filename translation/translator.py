ad3ff117c99b8dc62501d375eafa926b6219e4ce
lingy <lingy@i2r.a-star.edu.sg>
2023-11-02 22:30:37 +0800

    add general translation

--- translation/translator.py
+++ translation/translator.py
@@ -1,47 +1,53 @@
 import sys
 import json
 from tqdm import tqdm
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM,pipeline
+import googletrans
+import fire
+import multiprocessing as mp
 
-model_name=sys.argv[1]
-source_data_path=sys.argv[2]
-output_path = sys.argv[3]
+translator=googletrans.Translator()
+import time
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-translator=pipeline("translation",model=model,tokenizer=tokenizer)
+def translate_entry(args):
+    entry, target = args
+    success = False
+    new_entry = None
+    retry_count = 0
+    while not success and retry_count < 10:
+        try:
+            new_entry = {}
+            for key in entry.keys():
+                if len(entry[key]) == 0 or not entry[key]:
+                    new_entry[key] = entry[key]
+                else:
+                    new_entry[key] = translator.translate(entry[key], dest=target).text
+            success = True
+        except Exception as e:
+            time.sleep(1)
+            success=False
+            retry_count += 1
+    if not success:
+        raise Exception('Cannot translate {} after retrying'.format(entry))
 
+    return new_entry
 
+def translate(source, target_lang, output_path, num_workers=4):
+    with open(source, 'r') as f:
+        ds = json.load(f)
 
-def translate(scentence):
-    scentences=scentence.split('\n')
-    result=''
-    for i in range(len(scentences)):
-        if(len(scentences[i])!=0):
-            result+=translator(scentences[i])[0]['translation_text']
-        if(i!=len(scentences)-1):
-            result+='\n'
-    return result
+    translated = []
+    params = []
+    for entry in alpaca_ds:
+        params.append((entry, target_lang))
+    
+    with mp.Pool(num_workers) as p:
+        results = list(tqdm(p.imap(translate_entry, params), total=len(params)))
+    
+    translated = results
+    assert len(translated) == len(ds)
 
+    with open(output_path, 'w', encoding='utf-8') as f:
+        json.dump(translated, f, indent=1, ensure_ascii=False)
 
-
-with open(source_data_path) as f:
-    source_data=json.load(f)
-
-
-
-translated_data=[]
-for dic in tqdm(source_data):
-    temp={}
-    for key,value in dic.items():
-        if(len(value)==0):    
-            temp[key]=value
-            continue
-        temp[key]=translate(value)
-        print(temp[key])
-    translated_data.append(temp)
-
-
-
-with open(output_path,'w') as f:
-    json.dump(translated_data,f,ensure_ascii=False,indent=4)
+if __name__ == '__main__':
+    fire.Fire(translate)
