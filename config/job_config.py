797f030125df96f18a6ac432611a06c256b93f03
lingy <lingy@i2r.a-star.edu.sg>
2023-11-14 11:22:26 +0800

    update group

--- config/job_config.py
+++ config/job_config.py
@@ -39,3 +39,26 @@ alpaca_gpt4_group = {
     ],
     "prompt": ["general_prompt_no_sys"]
 }
+
+platypus_single_lang_group = {
+    "base_model": ["llama-2-7b-hf"],
+    "data": [
+          "platypus_en", 
+    "platypus_en+platypus_es",
+    "platypus_en+platypus_zh",
+    "platypus_en+platypus_vi",
+    "platypus_en+platypus_es+platypus_zh+platypus_vi"
+    ],
+    "prompt": ["general_prompt_no_sys"]
+}
+
+platypus_multi_lang_group = {
+    "base_model": ["llama-2-7b-hf"],
+    "data": [
+    "platypus_en+platypus_es",
+    "platypus_en+platypus_zh",
+    "platypus_en+platypus_vi",
+    "platypus_en+platypus_es+platypus_zh+platypus_vi"
+    ],
+    "prompt": ["general_prompt_no_sys"]
+}
