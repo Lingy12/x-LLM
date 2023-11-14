ca044f14f24e025d1f1d0ec7103fe035d5916007
lingy <lingy@i2r.a-star.edu.sg>
2023-11-14 11:24:09 +0800

    update single lang group

--- config/job_config.py
+++ config/job_config.py
@@ -44,10 +44,9 @@ platypus_single_lang_group = {
     "base_model": ["llama-2-7b-hf"],
     "data": [
           "platypus_en", 
-    "platypus_en+platypus_es",
-    "platypus_en+platypus_zh",
-    "platypus_en+platypus_vi",
-    "platypus_en+platypus_es+platypus_zh+platypus_vi"
+    "platypus_es",
+    "platypus_zh",
+    "platypus_vi",
     ],
     "prompt": ["general_prompt_no_sys"]
 }
